64759d23d6529a721f105ebacc63e710e5369fdc
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2025-02-18 13:14:11 +0100

    chore: initial commit, after generate empty project with Spring Initializr (with Spring Boot v3.4.2, Java v21 and Spring Web as dependency)

---
+++ src/main/java/com/juancff/mycontactsapi/MyContactsApiApplication.java
@@ -0,0 +1,13 @@
+package com.juancff.mycontactsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyContactsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyContactsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juancff/mycontactsapi/MyContactsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancff.mycontactsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyContactsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
