061df33559a611f523de574117356274e5b4acf0
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2025-02-21 14:06:13 +0100

    refactor: extract methods 'createContact_', 'getContactById' and 'getAllContacts'

--- src/main/java/com/juancff/mycontactsapi/ContactController.java
+++ src/main/java/com/juancff/mycontactsapi/ContactController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -19,7 +20,7 @@ public class ContactController {
 
     @PostMapping("/contacts")
     public ResponseEntity<ContactResponse> createContact(@RequestBody ContactRequest request) {
-        Contact contact = repository.save(request.name(), request.phoneNumber());
+        Contact contact = createContact_(request.name(), request.phoneNumber());
         var contactId = contact.id();
 
         URI location = URI.create("/contacts/" + contactId);
@@ -29,7 +30,7 @@ public class ContactController {
 
     @GetMapping("/contacts/{contactId}")
     public ResponseEntity<?> getContact(@PathVariable int contactId) {
-        Optional<Contact> optionalContact = repository.findById(contactId);
+        Optional<Contact> optionalContact = getContactById(contactId);
         if (optionalContact.isEmpty()) {
             Map<String, String> map = Map.of("error", "Contact not found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
@@ -42,8 +43,20 @@ public class ContactController {
 
     @GetMapping("/contacts")
     public List<ContactDetailsResponse> getContacts() {
-        return repository.findAll().stream()
+        return getAllContacts().stream()
             .map(contact -> new ContactDetailsResponse(contact.id(), contact.name(), contact.phoneNumber()))
             .toList();
+    }
+
+    private Contact createContact_(String name, String phoneNumber) {
+        return repository.save(name, phoneNumber);
+    }
+
+    private Optional<Contact> getContactById(int contactId) {
+        return repository.findById(contactId);
+    }
+
+    private Collection<Contact> getAllContacts() {
+        return repository.findAll();
     }
 }
